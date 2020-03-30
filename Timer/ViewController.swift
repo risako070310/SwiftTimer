5d456a7e6989779d72b380d6dd83d17b663f97ef
risako0703] <risako070310@gmail.com>
2020-03-30 16:45:09 +0900

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by 加藤りさ子 on 3/30/20.
+//  Copyright © 2020 Risako Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
